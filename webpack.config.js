6e07ffd9c266e03b4849eaa091c9cd34f2af399a
Freedteck <freedteck@gmail.com>
2023-10-28 00:21:03 +0100

    add webpack-web-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,16 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
   module: {
     rules: [
       {
